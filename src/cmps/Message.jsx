1ebbdbce17b6e00c1e456e614aade685667a01a9
haleli amiad <haleli.am@gmail.com>
2021-12-12 21:19:32 +0100

    lint rules

--- src/cmps/Message.jsx
+++ src/cmps/Message.jsx
@@ -1,11 +1,13 @@
 export default function Message({ message, author, onSendMacro }) {
-  const sendMacro = () => {
-    onSendMacro(message);
-  };
   const style =
     author && author === 'server'
       ? { backgroundColor: '#E75818', color: '#fff', alignSelf: 'flex-start' }
-      : { backgroundColor: '#fff', color: '#161616', alignSelf: 'flex-end'};
+      : { backgroundColor: '#fff', color: '#161616', alignSelf: 'flex-end' };
+
+  const sendMacro = () => {
+    !author ? onSendMacro(message) : null;
+  };
+
   return (
     <li className="message" style={style} onClick={sendMacro}>
       {message}
